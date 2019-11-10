63c15f41c3058f66bc67510ab909563fd3f21bfd
Luis Campos <lucam75@gmail.com>
2019-11-10 10:53:59 -0300

    Changes to the Create Shared Expense to allow create multiple expenses

--- force-app/main/default/lwc/createSharedExpense/createSharedExpense.js
+++ force-app/main/default/lwc/createSharedExpense/createSharedExpense.js
@@ -13,6 +13,9 @@ export default class CreateSharedExpense extends NavigationMixin(LightningElemen
     @track amount;
     @track paidBy;
     @track billedTo;
+    @track currentStep;
+
+    amountForDiana;
 
     @wire(getRecord, { recordId: '$UserId', fields: FIELDS })
     user;
@@ -20,12 +23,36 @@ export default class CreateSharedExpense extends NavigationMixin(LightningElemen
     constructor(){
         super();
         console.log('CreateSharedExpense');
-        this.amount = 100;
         this.paidBy = this.user;
-        this.billedTo = this.user;
-        console.log('user ' + this.user);
+        //this.billedTo = this.user;
+        //console.log('user ' + this.user);
+        this.currentStep = 1;
+    }
+    get renderStep1() {
+        return this.currentStep === 1;
+    }
+    get renderStep2() {
+        return this.currentStep === 2;
+    }
+    get renderStep3() {
+        return this.currentStep === 3;
+    }
+    get halfAccount() {
+        return this.amount / 2;
     }
     handleSuccess(){
         alert('success');
+    }
+    get comms() {
+        return [{ label: "Ross", value: "Option1" }, { label: "Rachel", value: "Option2" }];
+    }        
+    handleSelected(e) {
+        this.billedTo = e.detail.value;
+    }
+    nextStep(){
+        this.currentStep++;
+    }
+    prevStep(){
+        this.currentStep--;
     }
 }
