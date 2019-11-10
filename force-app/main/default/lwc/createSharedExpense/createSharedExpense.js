456ab83d9084e03242e2195852f72b763aca3233
Luis Campos <lucam75@gmail.com>
2019-11-10 09:55:16 -0300

    Updating metadata

--- force-app/main/default/lwc/createSharedExpense/createSharedExpense.js
+++ force-app/main/default/lwc/createSharedExpense/createSharedExpense.js
@@ -1,12 +1,29 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 import { NavigationMixin } from 'lightning/navigation';
+import UserId from '@salesforce/user/Id';
 
+const FIELDS = [
+    'User.Id',
+    'User.Name',
+];
 
 export default class CreateSharedExpense extends NavigationMixin(LightningElement) {
 
-    getAmount(){
-        console.log('getAmount');
-        return 100;
+    @track amount;
+    @track paidBy;
+    @track billedTo;
+
+    @wire(getRecord, { recordId: '$UserId', fields: FIELDS })
+    user;
+
+    constructor(){
+        super();
+        console.log('CreateSharedExpense');
+        this.amount = 100;
+        this.paidBy = this.user;
+        this.billedTo = this.user;
+        console.log('user ' + this.user);
     }
     handleSuccess(){
         alert('success');
