17f26373551d9d0b3db10b16d170165f63fb521f
Luis Campos <lucam75@gmail.com>
2020-06-29 13:48:27 -0400

    Adding features to the sliders

--- force-app/main/default/lwc/createSharedExpense/createSharedExpense.js
+++ force-app/main/default/lwc/createSharedExpense/createSharedExpense.js
@@ -19,217 +19,237 @@ import ACCOUNT_FIELD from '@salesforce/schema/Transaction__c.Account__c';
 
 export default class CreateSharedExpense extends NavigationMixin(LightningElement) {
 
-    @track amount;
-    @track paidBy;
-    @track currentStep;
-    @track billedToSelected = [''];
-    @track category;
-    @track date;
-    @track account;
-    @track event;
-    @track description;
+	@track amount;
+	@track paidBy;
+	@track currentStep = 1;
+	@track billedToSelected = [''];
+	@track category;
+	@track date;
+	@track account;
+	@track event;
+	@track description;
+	@track billedToSelectedList = [];
 
-    billedToAmounts = {};
-    createdTransactionsIds = [];
+	@api notifyViaAlerts = false;
+	@track errors = [];
 
-    @wire(getAllContacts)
-    contacts;
+	createdTransactionsIds = [];
+	showSliders = true;
+	@track manualMode = false;
 
-    connectedCallback(){
-        const now = new Date();
-        const offsetMs = now.getTimezoneOffset() * 60 * 1000;
-        const dateLocal = new Date(now.getTime() - offsetMs);
-        this.date = dateLocal.toISOString().slice(0, 19).replace(/-/g, "-").replace("T", " ");
+	@wire(getAllContacts)
+	contacts;
 
-        getLoggedContact()
-            .then(result => {
-                this.paidBy = result.Id;
-            })
-            .catch(error => {
-                /*eslint no-console: "error"*/
-                console.error(error);
-            });
-        this.currentStep = 1;
-    }
+	connectedCallback(){
+		// Set Date to Today
+		const now = new Date();
+		const offsetMs = now.getTimezoneOffset() * 60 * 1000;
+		const dateLocal = new Date(now.getTime() - offsetMs);
+		this.date = dateLocal.toISOString().slice(0, 19).replace(/-/g, "-").replace("T", " ");
 
-    get renderStep1() {
-        return this.currentStep === 1;
-    }
-    get renderStep2() {
-        return this.currentStep === 2;
-    }
-    get renderStep3() {
-        return this.currentStep === 3;
-    }
-    get dividedAmount() {
-        return this.amount / this.billedToSelected.length;
-    }
-    handleSuccess(){
-        alert('success');
-    }
-    handleSelected(e) {
-        this.billedToSelected = e.detail.value;
-    }
-    handlePaidBySelected(e){
-        this.paidBy = e.detail.value;
-    }
-    updateTrackedAmount(e) {
-        this.amount = e.detail.value;
-    }
-    
-    updateTrackedDate(e) {
-        this.date = e.detail.value;
-    }
+		getLoggedContact()
+			.then(result => {
+				this.paidBy = result.Id;
+			})
+			.catch(error => {
+				console.error(error);
+			});
+	}
 
-    updateTrackedDescription(e) {
-        this.description = e.detail.value;
-    }
-    contactLabel(contactId){
-        if(this.contacts.data !== undefined && contactId !== ''){
-            return this.contacts.data.find(contact => contact.Id === contactId).Name;
-        }
-        return '';
-    }
-    get billedToSelectedList(){
-        let billeds = [];
-        this.billedToSelected.forEach(billed => {
-            billeds.push({ 'label': this.contactLabel(billed), 'value': billed });
-        });
-        return billeds;
-    }
-    get contactsOptions() {
-        let contactsOptions = [];
-        if(this.contacts.data !== undefined){
-            this.contacts.data.forEach(val => {
-                contactsOptions.push({ 'label': val.Name, 'value': val.Id });
-            });
-        }
-        return contactsOptions;
-    }
-    nextStep(){
-        this.currentStep++;
-        if(this.currentStep === 3){
-            this.Step3DefaultValues();
-        }
-    }
-    prevStep(){
-        this.currentStep--;
-        if (this.currentStep === 2) {
-            if (this.category != null) {
-                let category_lookup = this.template.querySelectorAll('category_lookup');
-                console.log(JSON.stringify(category_lookup));
-                category_lookup.selection = this.category;
-            }
-        }
-    }
-    handleSliderChange(event) {
-        let index = event.target.dataset.item;
-        let value = event.detail.value;
-        this.billedToAmounts[index] = value;
-    }
-    Step3DefaultValues(){
-        this.billedToAmounts = {};
-        this.billedToSelected.forEach(contact => {
-            this.billedToAmounts[contact] = this.dividedAmount;
-        });
-    }
+	get renderStep1() {
+		return this.currentStep === 1;
+	}
 
-    handleCreateExpense(){
-        console.log('handleCreateExpense');
+	get renderStep2() {
+		return this.currentStep === 2;
+	}
 
-        for(let contact in this.billedToAmounts) {
-            if (Object.prototype.hasOwnProperty.call(this.billedToAmounts, contact)) {
-                let amount = this.billedToAmounts[contact];
-                this.createSingleTransaction(amount, contact);
-            }
-        }
-    }
+	get renderStep3() {
+		return this.currentStep === 3;
+	}
 
-    createSingleTransaction(amount, billedTo){
-        const fields = {};
-        fields[AMOUNT_FIELD.fieldApiName] = amount;
-        fields[BILLED_TO_FIELD.fieldApiName] = billedTo;
-        fields[CATEGORY_FIELD.fieldApiName] = this.category.id;
-        fields[DATE_FIELD.fieldApiName] = this.date;
-        fields[EVENT_FIELD.fieldApiName] = this.event.id;
-        fields[PAID_BY_FIELD.fieldApiName] = this.paidBy;
-        fields[DESCRIPTION_FIELD.fieldApiName] = this.description;
-        fields[ACCOUNT_FIELD.fieldApiName] = this.account.id;
+	get dividedAmount() {
+		return this.amount / this.billedToSelected.length;
+	}
 
-        const recordInput = { apiName: TRANSACTION_OBJECT.objectApiName, fields };
-        createRecord(recordInput)
-            .then(transaction => {
-                this.createdTransactionsIds.push(transaction.id);
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Shared transaction created',
-                        variant: 'success',
-                    }),
-                );
-            })
-            .catch(error => {
-                console.log(JSON.stringify(error));
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error creating shared transaction',
-                        message: error.body.message,
-                        variant: 'error',
-                    }),
-                );
-            });
-    }
+	get contactsOptions() {
+		let contactsOptions = [];
+		if(this.contacts.data !== undefined){
+			this.contacts.data.forEach(val => {
+				contactsOptions.push({ 'label': val.Name, 'value': val.Id });
+			});
+		}
+		return contactsOptions;
+	}
 
-    @api notifyViaAlerts = false;
+	// Handle field updates
+	handleBilledSelectedChange(e) {
+		this.billedToSelected = e.detail.value;
+		this.showSliders = this.billedToSelected.length > 1
+		this.updateBilledToSelectedList();
+	}
+	handlePaidBySelected(e){
+		this.paidBy = e.detail.value;
+	}
+	handleAmountChange(e) {
+		this.amount = e.detail.value;
+	}
+	handleDateChange(e) {
+		this.date = e.detail.value;
+	}
+	handleDescriptionChange(e) {
+		this.description = e.detail.value;
+	}
+	handleSliderChange(event) {
+		let sliderContactId = event.target.dataset.item;
+		let sliderContactAmount = event.detail.value;
+		this.billedToSelectedList.find(x => x.key === sliderContactId).value.amount = sliderContactAmount;
 
-    @track errors = [];
+		let calculatedNewAmount = (this.amount - sliderContactAmount) / (this.billedToSelectedList.length - 1);
 
-    handleSearch(event) {
-        let lookupType = event.target.getkey();
-        let parameters = event.detail;
-        
-        parameters.lookupType = lookupType;
-        const target = event.target;
+		this.billedToSelectedList.forEach(contact => {
+			if (contact.key != sliderContactId ) {
+				contact.value.amount = calculatedNewAmount;
+			}
+		});
+	}
+	handleManualAmountChange(event) {
+		let sliderContactId = event.target.dataset.item;
+		let sliderContactAmount = event.detail.value;
+		this.billedToSelectedList.find(x => x.key === sliderContactId).value.amount = sliderContactAmount;
+	}
+	handleCreateExpense(){
+		console.log('handleCreateExpense');
 
-        apexSearch(parameters)
-            .then(results => {
-                target.setSearchResults(results);
-            })
-            .catch(error => {
-                this.notifyUser(
-                    'Lookup Error',
-                    'An error occured while searching with the lookup field.',
-                    'error'
-                );
-                // eslint-disable-next-line no-console
-                console.error('Lookup error', JSON.stringify(error));
-                this.errors = [error];
-            });
-    }
+		console.log('this.billedToSelectedList.length ', this.billedToSelectedList.length);
+		if(this.billedToSelectedList.length > 1 ){
+			this.billedToSelectedList.forEach(contact => {
+				this.createSingleTransaction(contact.key, contact.value.amount);
+			});
+		}else if (this.billedToSelectedList.length == 1) {
+			this.createSingleTransaction(this.billedToSelectedList[0].key, this.billedToSelectedList[0].amount);
+		}else {
+			this.notifyUser('Error', 'Please select at least one contact to create the bill.', 'error');
+		}
+		
+	}
+	handleLookupSearch(event) {
+		let lookupType = event.target.getkey();
+		let parameters = event.detail;
+		
+		parameters.lookupType = lookupType;
+		const target = event.target;
 
-    handleSelectionChange(event) {
-        const lookupType = event.target.getkey();
-        const itemSelected = event.target.getSelection();
-        // [{"icon":"custom:custom46","id":"a003j00000TkrGJAAZ","sObjectType":"Category","subtitle":"","title":"Comida vegana"}]
+		apexSearch(parameters)
+			.then(results => {
+				target.setSearchResults(results);
+			})
+			.catch(error => {
+				this.notifyUser(
+					'Lookup Error',
+					'An error occured while searching with the lookup field.',
+					'error'
+				);
+				// eslint-disable-next-line no-console
+				console.error('Lookup error', JSON.stringify(error));
+				this.errors = [error];
+			});
+	}
 
-        if(lookupType == 'category_lookup'){
-            this.category = itemSelected[0];
-        }else if(lookupType == 'account_lookup'){
-            this.account = itemSelected[0];
-        }else if(lookupType == 'event_lookup'){
-            this.event = itemSelected[0];
-        }
-    }
+	handleLookupSelectionChange(event) {
+		const lookupType = event.target.getkey();
+		const itemSelected = event.target.getSelection();
+		// [{"icon":"custom:custom46","id":"a003j00000TkrGJAAZ","sObjectType":"Category","subtitle":"","title":"Comida vegana"}]
 
-    notifyUser(title, message, variant) {
-        if (this.notifyViaAlerts) {
-            // Notify via alert
-            // eslint-disable-next-line no-alert
-            alert(`${title}\n${message}`);
-        } else {
-            // Notify via toast
-            const toastEvent = new ShowToastEvent({ title, message, variant });
-            this.dispatchEvent(toastEvent);
-        }
-    }
+		if(lookupType == 'category_lookup'){
+			this.category = itemSelected[0];
+		}else if(lookupType == 'account_lookup'){
+			this.account = itemSelected[0];
+		}else if(lookupType == 'event_lookup'){
+			this.event = itemSelected[0];
+		}
+	}
+
+	handleManualModeChange(event) {
+		console.log('handleManualModeChange ');
+		this.manualMode = event.target.checked;
+	}
+
+	// Navigation events
+	nextStep(){
+		this.currentStep++;
+		if(this.currentStep === 3){
+			this.step3Logic();
+		}
+	}
+	prevStep(){
+		this.currentStep--;
+		if (this.currentStep === 2) {
+			if (this.category != null) {
+				let category_lookup = this.template.querySelectorAll('category_lookup');
+				console.log(JSON.stringify(category_lookup));
+				category_lookup.selection = this.category;
+			}
+		}
+	}
+
+	// Utilitary methods
+	getContactLabel(contactId){
+		if(this.contacts.data !== undefined && contactId !== ''){
+			return this.contacts.data.find(contact => contact.Id === contactId).Name;
+		}
+		return '';
+	}
+
+	step3Logic(){
+		this.showSliders = true;
+	}
+
+	updateBilledToSelectedList() {
+		console.log('updateBilledToSelectedList ');
+		let tempBilledToSelectedList = [];
+		this.billedToSelected.forEach(billed => {
+			let added = { key: billed, value: { 'label': this.getContactLabel(billed), 'contactId': billed, 'amount' : this.dividedAmount }};
+            tempBilledToSelectedList.push(added);
+		});
+
+		this.billedToSelectedList = tempBilledToSelectedList;		
+	}
+
+	createSingleTransaction(billedTo, amount){
+		console.log('createSingleTransaction');
+		const fields = {};
+		fields[AMOUNT_FIELD.fieldApiName] = amount;
+		fields[BILLED_TO_FIELD.fieldApiName] = billedTo;
+		fields[CATEGORY_FIELD.fieldApiName] = this.category.id;
+		fields[DATE_FIELD.fieldApiName] = this.date;
+		fields[EVENT_FIELD.fieldApiName] = this.event != undefined ? this.event.id : '';
+		fields[PAID_BY_FIELD.fieldApiName] = this.paidBy;
+		fields[DESCRIPTION_FIELD.fieldApiName] = this.description;
+		fields[ACCOUNT_FIELD.fieldApiName] = this.account != undefined ? this.account.id : '';
+
+		const recordInput = { apiName: TRANSACTION_OBJECT.objectApiName, fields };
+		console.log('recordInput ', recordInput);
+		/*createRecord(recordInput)
+			.then(transaction => {
+				this.createdTransactionsIds.push(transaction.id);
+				this.notifyUser('Success', 'Shared transaction created', 'success');
+			})
+			.catch(error => {
+				console.log(JSON.stringify(error));
+				this.notifyUser('Error creating shared transaction', error.body.message, 'error');
+			});*/
+	}
+
+	notifyUser(title, message, variant) {
+		if (this.notifyViaAlerts) {
+			// Notify via alert
+			// eslint-disable-next-line no-alert
+			alert(`${title}\n${message}`);
+		} else {
+			// Notify via toast
+			const toastEvent = new ShowToastEvent({ title, message, variant });
+			this.dispatchEvent(toastEvent);
+		}
+	}
 }
