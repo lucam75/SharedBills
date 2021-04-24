85eebb68e17a4c4558d7ba8f7fd2b04247626602
Luis Campos <lucam75@gmail.com>
2021-04-24 13:41:20 -0400

    Updating dependecies.

--- scripts/bash/initdev.bash
+++ scripts/bash/initdev.bash
@@ -3,7 +3,7 @@ echo 'Scratch org alias?'
 read orgAlias
 
 echo 'Creating Scratch Org'
-sfdx force:org:create  -f config/project-scratch-def.json -a $orgAlias -u CuentasDevHub -d 30
+sfdx force:org:create  -f ../../config/project-scratch-def.json -a $orgAlias -u CuentasDevHub -d 30
 
 echo "Pushing code to scratch org ..."
 sfdx force:source:push -u $orgAlias
@@ -12,7 +12,7 @@ echo "Adding permission set ..."
 sfdx force:user:permset:assign -n Shared_Bills_User -u $orgAlias
 
 echo "Creating data ..."
-sfdx force:data:tree:import -u $orgAlias -p data/Sample-plan.json
+sfdx force:data:tree:import -u $orgAlias -p ../../data/Sample-plan.json
 
 echo "Opening Scratch Org"
 sfdx force:org:open -u $orgAlias
