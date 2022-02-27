67411565c3a8682a32f27c4f87ae8384c4cf8a4e
Luis Campos <lucam75@gmail.com>
2022-02-27 13:16:03 -0300

    Transaction Historical implementation

--- scripts/bash/newversion.bash
+++ scripts/bash/newversion.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-sfdx force:package:version:create -p "Shared Bills" -k password123 -v CuentasDevHub -w 20 -t "Including extended bill form"
+sfdx force:package:version:create -p "Shared Bills" -k password123 -v CuentasDevHub -w 20 -t "Transaction Historical"
